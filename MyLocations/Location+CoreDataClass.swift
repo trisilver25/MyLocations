0f6721844f126d0c9722e68b445ddfa47541a4e6
Tristin Smith <tsmithassoc@gmail.com>
2024-02-13 20:21:46 -0500

    Updates: - Imported CoreData and set up attributes. - Added an alert that catches an error and displays it to the user. - Restructed code.

---
+++ MyLocations/Location+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Location+CoreDataClass.swift
+//  MyLocations
+//
+//  Created by Tristin Smith on 2/13/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Location)
+public class Location: NSManagedObject {
+
+}
---
+++ MyLocations/Location+CoreDataProperties.swift
@@ -0,0 +1,30 @@
+//
+//  Location+CoreDataProperties.swift
+//  MyLocations
+//
+//  Created by Tristin Smith on 2/13/24.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Location {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Location> {
+        return NSFetchRequest<Location>(entityName: "Location")
+    }
+
+    @NSManaged public var latitude: Double
+    @NSManaged public var longitude: Double
+    @NSManaged public var date: Date?
+    @NSManaged public var locationDescription: String?
+    @NSManaged public var category: String?
+    @NSManaged public var placemark: NSObject?
+
+}
+
+extension Location : Identifiable {
+
+}
